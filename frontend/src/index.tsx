aa45b08266183c776a656a1405ae85e4a4077abe
Eirik Sander-Fjeld <eisande@equinor.com>
2022-06-02 23:42:04 +0200

    Logging out config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -54,6 +54,7 @@ registerApp('conceptapp', {
     context: {
         types: [ContextTypes.ProjectMaster],
         buildUrl: (context: Context | null, url: string) => {
+            console.log(RetrieveConfigFromAzure)
             console.log("Context: ", context)
             if (!context) return ''
             return `/${context.id}`
